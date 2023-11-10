9ef281a2c1f3b833abc2dbaa95249d5065a5ca1a
nndang1229 <nndang.1229@indivisys.jp>
2023-11-10 16:16:13 +0700

    connected mongodb

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,11 +1,23 @@
+import connect from "@/utils/db";
 import { NextResponse } from "next/server";
-import connect from "../../../../db";
 
-export const GET = async () => {
+export const GET = async (req: any, res: any) => {
   try {
+    // Kết nối đến MongoDB
     await connect();
+
+    // Logic xử lý cho route handler ở đây
+
+    // Trả về một Response thành công
+    return new NextResponse("successfully: ", {
+      status: 200,
+    });
   } catch (error) {
-    return new NextResponse("Error in fetching posts: " + error, {
+    // Xử lý lỗi kết nối MongoDB
+    console.log("Mongo connection failed!", error);
+
+    // Trả về một NextResponse với mã trạng thái 500
+    return new NextResponse("Error in fetching data: " + error, {
       status: 500,
     });
   }
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    mongoose.connect(process.env.MONGO ?? "", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(process.env.MONGO_URL ?? "");
     console.log("Mongo connection successful!");
   } catch (error) {
     throw new Error("Connection failed!");
