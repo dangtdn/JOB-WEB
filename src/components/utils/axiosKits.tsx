b04fe2c9699d720f36136bba983d730a52e0d6f0
Nguyen Nhat Dang(IVS) <nndang.0227@vitenet.net>
2024-01-19 00:38:42 +0700

    update

--- src/components/utils/axiosKits.tsx
+++ src/components/utils/axiosKits.tsx
@@ -2,11 +2,11 @@ import axios from "axios";
 import { localGet } from "./localStore";
 
 export const Axios = axios.create({
-  baseURL: `/api`,
+  baseURL: `http://localhost:8000/api`,
 });
 
 export const authAxios = axios.create({
-  baseURL: `/api`,
+  baseURL: `http://localhost:8000/api`,
   headers: {
     Authorization: `Bearer ${localGet("UserData")?.accessToken}`,
     Accept: "application/json",
