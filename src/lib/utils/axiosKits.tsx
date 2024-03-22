2e07a05e42289e134bee542759ddf8e865a439f5
Nguyen Nhat Dang(IVS) <nndang.0227@vitenet.net>
2024-03-22 20:54:37 +0700

    update url API deploy

--- src/lib/utils/axiosKits.tsx
+++ src/lib/utils/axiosKits.tsx
@@ -2,11 +2,11 @@ import axios from "axios";
 import { localGet } from "./localStore";
 
 export const Axios = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `https://job-web-be.vercel.app/api`,
 });
 
 export const authAxios = axios.create({
-  baseURL: `http://localhost:8000/api`,
+  baseURL: `https://job-web-be.vercel.app/api`,
   headers: {
     Authorization: `Bearer ${localGet("UserData")?.token}`,
     Accept: "application/json",
